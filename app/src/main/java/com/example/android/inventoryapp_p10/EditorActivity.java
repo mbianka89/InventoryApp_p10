3eab925f54cdfd977a63570e5952e63356175e4e
mbianka89 <matyas.bianka28@gmail.com>
2017-07-19 13:33:59 +0300

    Fab & menu options

--- app/src/main/java/com/example/android/inventoryapp_p10/EditorActivity.java
+++ app/src/main/java/com/example/android/inventoryapp_p10/EditorActivity.java
@@ -1,9 +1,17 @@
 package com.example.android.inventoryapp_p10;
 
+import android.os.Bundle;
+
 /**
  * Created by Bianka Matyas on 19/07/2017.
  */
 
 public class EditorActivity {
 
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.editor_activity);
+
+    }
 }
